6c0ca495f02b4af9b70809efc0386c85ed6a1636
Asterios Raptis <asterios.raptis@gmx.net>
2016-04-26 15:40:03 +0200

    new shell script for import git repositories
    shell script for import git repositories that reads the settings from a
    file.

---
+++ src/main/resources/git/impgitrep-from-file-bitbucket.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+# import remote git repositories to local git dir
+
+# import variables from file.
+source impgitrep-settings-bitbucket.sh
+
+# Echoes all commands before executing.
+set -o verbose
+
+# change to git directory
+cd $localCodeDir
+pwd
+# iterate over the repository names
+for gitRepositoryName in $gitRepositoryNames
+
+do
+	# create a string from the local repository directory for checks
+	localRepoDir=$(echo ${localCodeDir}${gitRepositoryName})
+	# check if the local repository directory already exists
+	if [[ -d $localRepoDir ]]; then 
+		# if yes skip and continue the iteration
+		echo -e "Directory $localRepoDir already exits, skipping ...\n"
+	else 
+		# if not then clone it and check out the develop branch  
+		echo -e "Directory $localRepoDir does not exits, start clone repository ...\n"
+		echo -e "Running clone command: $cloneCmd \n"
+		# git clone 
+		git clone https://$remoteUser@$remoteHost/$remoteUser/$gitRepositoryName.git
+		cd $gitRepositoryName 
+		git checkout develop
+		cd $localCodeDir			
+	fi
+done
---
+++ src/main/resources/git/impgitrep-settings-bitbucket.sh
@@ -0,0 +1,12 @@
+# settings 
+remoteHost=bitbucket.org
+remoteUser=foobar
+localCodeDir="${HOME}/git/"
+gitRepositoryNames=(
+"alpharogroup.com"
+"lessonslearn.com"
+"xmas.presents"
+"wingames"
+"Spirit-Intensifier"
+"BackupContactLinks"
+)
