4d068423fb561cee611846874ee8e94f603b4d5f
Asterios Raptis <asterios.raptis@gmx.net>
2017-01-10 15:32:08 +0100

    Update start-eclipse.bat

--- src/main/resources/eclipse/start/start-eclipse.bat
+++ src/main/resources/eclipse/start/start-eclipse.bat
@@ -1,12 +1,8 @@
-set IDES_HOME=%HOME%/ides
-set ECLIPSE_VERSION_NAME=lunajee
-set ECLIPSE_HOME=
-set IDES_HOME=%HOME%/ides
-set ECLIPSE_VERSION_NAME=lunajee
-set ECLIPSE_HOME=%IDES_HOME%/eclipse/%ECLIPSE_VERSION_NAME%/eclipse
-set WORKSPACES_HOME=%HOME%/workspaces
-set WORKSPACE_HOME=%WORKSPACES_HOME%/jaulp.core
-set ECLIPSE_EXECUTABLE=%ECLIPSE_HOME%/eclipse
-set JAVA_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/jni/libsvnjavahl-1.so
-set VM_ARGS=-Xms384m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=512M
-%ECLIPSE_EXECUTABLE% /i /max /high -data %WORKSPACE_HOME% -showlocation vmargs %VM_ARGS% -javaagent:%ECLIPSE_HOME%/lombok.jar -Xbootclasspath/a:%ECLIPSE_HOME%/lombok.jar -Djava.library.path=%JAVA_LIBRARY_PATH%IDES_HOME%/eclipse/%ECLIPSE_VERSION_NAME%/eclipse
+set IDES_HOME="%HOME%/ides"
+set ECLIPSE_VERSION_NAME="lunajee"
+set ECLIPSE_HOME="%IDES_HOME%/eclipse/%ECLIPSE_VERSION_NAME%/eclipse"
+set WORKSPACES_HOME="%HOME%/wss"
+set WORKSPACE_HOME="%WORKSPACES_HOME%/jaulp-core"
+set ECLIPSE_EXECUTABLE="%ECLIPSE_HOME%/eclipse.exe"
+set VM_ARGS="-Xms384m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=512M"
+%ECLIPSE_EXECUTABLE% /i /max /high -data %WORKSPACE_HOME% -showlocation vmargs %VM_ARGS%
