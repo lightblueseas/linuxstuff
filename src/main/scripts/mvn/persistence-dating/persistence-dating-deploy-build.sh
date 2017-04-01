b7b0c88ab6c79f059a4cdcfe4010acdb16896828
Asterios Raptis <asterios.raptis@gmx.net>
2017-04-01 12:15:49 +0200

    new shell script for deployment

---
+++ src/main/scripts/mvn/persistence-dating/persistence-dating-deploy-build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# import settings for the ext-lib projects for building.
+# import variables from file.
+source persistence-dating-settings.sh
+
+# Echoes all commands before executing.
+set -o verbose
+
+# change to git directory
+cd $localCodeDir
+pwd
+# iterate over the repository names
+for projectName in $projectNames
+
+do
+	# create a string from the local project directory for checks
+	localProjectDir=$(echo ${localCodeDir}${projectName})
+	# check if the local repository directory already exists
+	if [[ -d $localProjectDir ]]; then 
+		# if yes execute mvn command
+		cd $projectName
+		mvn -Pupdate-license-header -B --threads 8 clean license:format
+		mvn -B -Dmaven.test.skip=true -DskipTests --threads 8 javadoc:javadoc
+		mvn -B -Dmaven.test.skip=true -DskipTests --threads 8 clean install
+		mvn -Poss.sonatype.org-staged-release -B --threads 8 clean deploy --settings $mvnSettingsXmlDir
+		cd $localCodeDir			
+	else 
+		cloneCmd="git clone https://$remoteUser@$remoteHost/$remoteDir/$projectName.git"
+		# if not then clone it and check out the develop branch  
+		echo -e "Directory $localProjectDir does not exits, start clone repository ...\n"
+		echo -e "Running clone command: $cloneCmd \n"
+		# git clone 
+		git clone https://$remoteUser@$remoteHost/$remoteUser/$projectName.git
+		cd $projectName 
+		git checkout develop
+		mvn -Pupdate-license-header -B --threads 8 clean license:format
+		mvn -B -Dmaven.test.skip=true -DskipTests --threads 8 javadoc:javadoc
+		mvn -B -Dmaven.test.skip=true -DskipTests --threads 8 clean install
+		mvn -Poss.sonatype.org-staged-release -B --threads 8 clean deploy --settings $mvnSettingsXmlDir
+		cd $localCodeDir			
+	fi
+done
--- src/main/scripts/mvn/persistence-dating/persistence-dating-settings.sh
+++ src/main/scripts/mvn/persistence-dating/persistence-dating-settings.sh
@@ -2,4 +2,5 @@
 remoteHost=github.com
 remoteUser=lightblueseas
 localCodeDir="${HOME}/git/"
+mvnSettingsXmlDir="${HOME}/.m2/settings.xml"
 projectNames=("persistence-api user-data address-book-data resource-system-data resource-bundles-data user-management-data message-system-data dating-system-data")
