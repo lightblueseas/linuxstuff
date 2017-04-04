5417c52a7bb4e27c176c88a4bb442ae7aa23969f
Leonidas Boubos <jim.macenroe@gmail.com>
2017-04-04 09:34:58 +0200

    Added gpg.passphrase
    if it is set you do not need to give gpg.passphrase.

--- src/main/scripts/mvn/persistence-dating/persistence-dating-settings.sh
+++ src/main/scripts/mvn/persistence-dating/persistence-dating-settings.sh
@@ -3,4 +3,5 @@ remoteHost=github.com
 remoteUser=lightblueseas
 localCodeDir="${HOME}/git/"
 mvnSettingsXmlDir="${HOME}/.m2/settings.xml"
+gpgPassphrase="YOUR_PASSPHRASE"
 projectNames=("persistence-api user-data address-book-data resource-system-data resource-bundles-data user-management-data message-system-data dating-system-data")
