aa46a691e0d583692a41b344a62a2f1797c4e82c
Asterios Raptis <asterios.raptis@gmx.net>
2017-04-20 18:23:48 +0200

    new readme file created

--- src/main/scripts/mvn/persistence-dating/persistence-dating-settings.sh
+++ src/main/scripts/mvn/persistence-dating/persistence-dating-settings.sh
@@ -4,4 +4,4 @@ remoteUser=lightblueseas
 localCodeDir="${HOME}/git/"
 mvnSettingsXmlDir="${HOME}/.m2/settings.xml"
 gpgPassphrase="YOUR_PASSPHRASE"
-projectNames=("persistence-api user-data address-book-data resource-system-data resource-bundles-data user-management-data message-system-data dating-system-data")
+projectNames=("persistence-api user-data address-book-data resource-system-data resource-bundles-data user-management-data message-system-data rating-system-data dating-system-data")
