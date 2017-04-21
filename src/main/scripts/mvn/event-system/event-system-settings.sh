36ab79b31fb94d03e6e65a81d3daec1fe6b4d2cd
Asterios Raptis <asterios.raptis@gmx.net>
2017-04-21 15:45:59 +0200

    created settings for event-system-data project

---
+++ src/main/scripts/mvn/event-system/event-system-settings.sh
@@ -0,0 +1,7 @@
+# settings 
+remoteHost=github.com
+remoteUser=lightblueseas
+localCodeDir="${HOME}/git/"
+mvnSettingsXmlDir="${HOME}/.m2/settings.xml"
+gpgPassphrase="YOUR_PASSPHRASE"
+projectNames=("persistence-api scheduler-system-data user-data address-book-data resource-system-data resource-bundles-data user-management-data message-system-data rating-system-data event-system-data")
