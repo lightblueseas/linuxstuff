382e9a8d197cfa5b10f42f37332f9a8326ec9880
Asterios Raptis <asterios.raptis@gmx.net>
2017-05-12 20:17:57 +0200

    update to pull script

--- src/main/resources/git/pullgitrep-from-file.sh
+++ src/main/resources/git/pullgitrep-from-file.sh
@@ -18,16 +18,15 @@ do
 	localRepoDir=$(echo ${localCodeDir}${gitRepositoryName}|cut -d'.' -f1)
 	# check if the local repository directory already exists
 	if [[ -d $localRepoDir ]]; then 
-		# if yes skip and continue the iteration
-		echo -e "Directory $localRepoDir already exits, skipping ...\n"
-	else 
-		cloneCmd="git clone https://$remoteUser@$remoteHost/$remoteDir/$projectName.git"
-		# if not then clone it and check out the develop branch  
-		echo -e "Directory $localRepoDir does not exits, start clone repository ...\n"
-		echo -e "Running clone command: $cloneCmd \n"
-		git clone https://$remoteHost/$remoteUser/$gitRepositoryName.git
+		# if yes change to it and pull
+		echo -e "Directory $localRepoDir exits, change to it ...\n"
+		pullCmd="git pull" 
+		echo -e "Running pull command: $pullCmd \n"
 		cd $gitRepositoryName 
-		git checkout develop
-		cd $localCodeDir			
+		git checkout develop		
+		git pull
+		cd ..	
+	else 
+				
 	fi
 done
