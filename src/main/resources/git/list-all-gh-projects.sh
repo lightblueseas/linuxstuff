661430f04d62d1d002fe8dda923dc3473a06f25c
Asterios Raptis <asterios.raptis@gmx.net>
2017-05-13 18:47:37 +0200

    update of gh repositories

--- src/main/resources/git/list-all-gh-projects.sh
+++ src/main/resources/git/list-all-gh-projects.sh
@@ -1,2 +1,7 @@
-USER=YOUR_USER; 
-curl -s "https://api.github.com/users/$USER/repos?per_page=1000" | grep -o 'git@[^"]*'| cut -d'/' -f2 > repolist.txt
+#!/bin/zsh
+# export all public remote git repositories from the given user to given file path with file name
+echo 'Insert your github username:'
+read USER 
+echo 'Insert the file name to export the remote git repositories from the given user:'
+read TO_FILE
+curl -s "https://api.github.com/users/$USER/repos?per_page=1000" | grep -o 'git@[^"]*'| cut -d'/' -f2 > $TO_FILE
