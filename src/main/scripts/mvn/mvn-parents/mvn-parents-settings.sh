a8b5bacfc16b7d479bc9be705dff7d20b6684375
Asterios Raptis <asterios.raptis@gmx.net>
2017-05-27 12:28:40 +0200

    new scripts created for get mvn-parents and mystic-crypt

---
+++ src/main/scripts/mvn/mvn-parents/mvn-parents-settings.sh
@@ -0,0 +1,7 @@
+# settings 
+remoteHost=github.com
+remoteUser=lightblueseas
+localCodeDir="${HOME}/git/"
+mvnSettingsXmlDir="${HOME}/.m2/settings.xml"
+gpgPassphrase="YOUR_PASSPHRASE"
+projectNames=("mvn-parent-projects mvn-java-parent mvn-less-css mvn-ui-parent mvn-persistence-parent mvn-wicket-parent mvn-wicket-application-parent")
--- src/main/scripts/mvn/mystic-crypt/mystic-crypt-get-repos.sh
+++ src/main/scripts/mvn/mystic-crypt/mystic-crypt-get-repos.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# This script gets all projects and change to the develop branch makes a git pull
+# and builds. If the project is not on local disk it get it from the remote host.
+# 
+
 # import settings for the ext-lib projects for building.
 # import variables from file.
 source mystic-crypt-settings.sh
@@ -14,14 +19,18 @@ for projectName in $projectNames
 
 do
 	# create a string from the local project directory for checks
-	localProjectDir=$(echo ${localCodeDir}${projectName})
+	# will be displayed in console and is irritating i know...
+	localProjectDir=$(echo ${localCodeDir}${projectName}) 
+	echo -e "var: $localProjectDir is the current project directory ...\n"
 	# check if the local repository directory already exists
 	if [[ -d $localProjectDir ]]; then 
 		# if yes execute mvn command
 		cd $projectName
+		git checkout develop
+		git pull
 		mvn -B -Dmaven.test.skip=true -DskipTests --threads 8 clean install
 		cd $localCodeDir			
-	else 
+	else 		
 		cloneCmd="git clone https://$remoteUser@$remoteHost/$remoteDir/$projectName.git"
 		# if not then clone it and check out the develop branch  
 		echo -e "Directory $localProjectDir does not exits, start clone repository ...\n"
