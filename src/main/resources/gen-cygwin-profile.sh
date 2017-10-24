a4d58f3ce61778fbfe3190bea19eefb7bbd7d4de
James Phelps <tyler.durden69@gmx.de>
2017-10-24 14:57:42 +0200

    Add files via upload

---
+++ src/main/resources/gen-cygwin-profile.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+# merge defragmented aliases files to the '.cygwinprofile'. 
+# This file can than merged with the '.profile' for the cygwin home directory.
+cat .bashhistory .cygwindir .git-aliases .mvnrc .zipping > .cygwinprofile
