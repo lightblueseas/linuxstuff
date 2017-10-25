5464421533ede1d171e8303013017fa54e4cf322
Jacqueline Morin <jacqueline.morin@gmx.net>
2017-10-25 13:20:08 +0200

    Add files via upload

--- src/main/resources/gen-profile.sh
+++ src/main/resources/gen-profile.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 # merge defragmented aliases files to the '.profile'. 
 # This file can than merged with the '.profile' for the users home directory.
 cat .bowerrc .dirrc .git-aliases .gulprc .mvnrc .npmrc .tweak.sh .zipping > .profile
