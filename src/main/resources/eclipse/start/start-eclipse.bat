93ffa4d3b58fbb0d1bf47f083cbf6d0f095b28bb
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2017-12-13 17:42:38 +0100

    added jdk executable

--- src/main/resources/eclipse/start/start-eclipse.bat
+++ src/main/resources/eclipse/start/start-eclipse.bat
@@ -1,8 +1,11 @@
 set IDES_HOME="%HOME%/ides"
+set JDKS_HOME=%DEV_HOME%\jdks
+set JDK8_HOME=%JDKS_HOME%\jdk1.8.0_151
+set JDK8_EXECUTABLE=%JDK8_HOME%\bin\javaw.exe
 set ECLIPSE_VERSION_NAME="lunajee"
 set ECLIPSE_HOME="%IDES_HOME%/eclipse/%ECLIPSE_VERSION_NAME%/eclipse"
 set WORKSPACES_HOME="%HOME%/wss"
 set WORKSPACE_HOME="%WORKSPACES_HOME%/jaulp-core"
 set ECLIPSE_EXECUTABLE="%ECLIPSE_HOME%/eclipse.exe"
 set VM_ARGS="-Xms384m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=512M"
-%ECLIPSE_EXECUTABLE% /i /max /high -data %WORKSPACE_HOME% -showlocation vmargs %VM_ARGS%
+%ECLIPSE_EXECUTABLE% /i /max /high -data %WORKSPACE_HOME% -showlocation -vm %JDK8_EXECUTABLE% vmargs %VM_ARGS%
