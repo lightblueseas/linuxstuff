80bcbf94c6843540a29ec3e114218d34012da952
Asterios Raptis <asterios.raptis@gmx.net>
2018-02-16 17:32:55 +0100

    created new file for shell aliases

--- src/main/resources/gen-cygwin-profile.sh
+++ src/main/resources/gen-cygwin-profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # merge defragmented aliases files to the '.cygwinprofile'. 
 # This file can than merged with the '.profile' for the cygwin home directory.
-cat .bashhistory .cygwindir .git-aliases .mvnrc .zipping > .cygwinprofile
+cat .bashhistory .cygwindir .git-aliases .mvnrc .shell-aliases .zipping > .cygwinprofile
--- src/main/resources/gen-profile.sh
+++ src/main/resources/gen-profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # merge defragmented aliases files to the '.profile'. 
 # This file can than merged with the '.profile' for the users home directory.
-cat .aliasesrc .bowerrc .dirrc .git-aliases .gulprc .mvnrc .npmrc .tweak.sh .zipping > .profile
+cat .aliasesrc .bowerrc .dirrc .git-aliases .gulprc .mvnrc .npmrc .shell-aliases .tweak.sh .zipping > .profile
