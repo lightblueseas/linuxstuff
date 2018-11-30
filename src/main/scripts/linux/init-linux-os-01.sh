fc3be3f9d72e242562565dd2a10d98e49676edbf
Asterios Raptis <asterios.raptis@gmx.net>
2018-11-30 23:06:01 +0100

    created new initial scripts after virgin install of ubuntu

---
+++ src/main/scripts/linux/init-linux-os-01.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get install clipit
+sudo apt install zsh
+zsh --version
+chsh -s $(which zsh)
+echo $SHELL
---
+++ src/main/scripts/linux/init-linux-os-02.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install default-jdk
+java -version
+sudo apt-get install maven
+echo $JAVA_HOME
+sudo echo "JAVA_HOME=/usr/lib/jvm/default-java" >> /etc/environment
+echo $JAVA_HOME
+sudo apt-get install jedit
+jedit
+cd ~
+mkdir ides && mkdir ~/ides/eclipse && mkdir ~/ides/netbeans && mkdir ~/ides/jedit && mkdir ~/ides/androidstudios
+mkdir ~/wss && mkdir ~/dev  && mkdir ~/dev/servers && mkdir ~/tmp && mkdir ~/apps
+sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
+sudo apt-get install chromium-codecs-ffmpeg-extra
