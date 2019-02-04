61292fffe8b067f652d2c333f187fa08c54fba26
Asterios Raptis <asterios.raptis@gmx.net>
2019-02-04 17:54:09 +0100

    Update .tweak.sh

--- src/main/resources/.tweak.sh
+++ src/main/resources/.tweak.sh
@@ -5,8 +5,9 @@ cleanup() {
    sudo apt-get autoclean
    sudo apt-get clean
    sudo apt-get autoremove
-   sudo apt-get update
-   sudo apt-get upgrade
+   sudo apt-get update -y
+   sudo apt-get upgrade -y
+   sudo apt-get dist-upgrade
    sudo apt --fix-broken install
    rm -rf ~/.cache/thumbnails/*
 }
