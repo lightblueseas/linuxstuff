617bb2d7d0fae569a052584d494257fb85f2a107
Asterios Raptis <asterios.raptis@gmx.net>
2019-02-08 17:07:54 +0100

    added thunderbird

--- src/main/scripts/linux/init-linux-os-02.sh
+++ src/main/scripts/linux/init-linux-os-02.sh
@@ -15,3 +15,6 @@ sudo apt-get install chromium-codecs-ffmpeg-extra
 sudo add-apt-repository ppa:jtaylor/keepass
 sudo apt-get update
 sudo apt-get install keepass2
+sudo apt-get install ppa-purge && sudo ppa-purge ppa:jtaylor/keepass
+sudo apt-get install thunderbird
+sudo apt-get install thunderbird-locale-de
