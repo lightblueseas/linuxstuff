1be69fe1450ffe8e28a26a09406f59405ebf2aca
astrapi69 <asterios.raptis@gmx.net>
2019-02-08 17:35:51 +0100

    update command for set JAVA_HOME

--- src/main/scripts/linux/init-linux-os-02.sh
+++ src/main/scripts/linux/init-linux-os-02.sh
@@ -13,13 +13,12 @@ sudo apt install default-jdk
 java -version
 sudo apt-get install maven
 echo $JAVA_HOME
-sudo awk 'BEGIN{ printf "JAVA_HOME=\"/usr/lib/jvm/java-8-oracle\"" >> "/etc/environment"  }'
-sudo echo "JAVA_HOME=/usr/lib/jvm/default-java" >> /etc/environment
+sudo awk 'BEGIN{ printf "JAVA_HOME=\"/usr/lib/jvm/default-java\"" >> "/etc/environment"  }'
 sudo source /etc/environment
 echo $JAVA_HOME
 sudo apt-get install jedit
 cd ~
-mkdir ides && mkdir ~/ides/eclipse && mkdir ~/ides/netbeans && mkdir ~/ides/jedit && mkdir ~/ides/androidstudios
+mkdir ides && mkdir ~/ides/eclipse && mkdir ~/ides/netbeans && mkdir ~/ides/jedit && mkdir ~/ides/androidstudios && mkdir ~/ides/intellij
 mkdir ~/wss && mkdir ~/dev  && mkdir ~/dev/servers && mkdir ~/tmp && mkdir ~/apps
 sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
 sudo apt-get install chromium-codecs-ffmpeg-extra
