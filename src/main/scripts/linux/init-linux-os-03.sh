7f933f9e06118464d7a7a4e7bbde288191ba2a5b
Asterios Raptis <asterios.raptis@gmx.net>
2019-05-22 12:31:44 +0200

    Create init-linux-os-03.sh

---
+++ src/main/scripts/linux/init-linux-os-03.sh
@@ -0,0 +1,23 @@
+sudo apt-get install gnupg-agent pinentry-gtk2
+sudo apt-get install ktouch
+sudo apt-get install shotwell
+sudo apt-get install gnome-system-tools
+sudo apt-get install nautilus-actions
+sudo apt-get install nautilus-gksu
+sudo apt-get install nautilus-actions-extra
+sudo apt-get install gnome-sushi 
+sudo apt-get install nautilus-admin nautilus-sendto nautilus-image-converter nautilus-open-terminal nautilus-compare nautilus-wipe seahorse-nautilus nautilus-gtkhash nautilus-share
+sudo apt-get install ffmpegthumbnailer 
+sudo apt-get install nautilus-script-manager
+# nodejs
+sudo apt install nodejs npm
+# antivirus programm clamav
+sudo apt-get install clamav clamav-freshclam 
+sudo apt-get install clamav-docs
+sudo freshclam
+sudo apt-get install clamav-daemon 
+sudo apt-get install clamtk 
+sudo apt-get install clamtk-nautilus
+sudo apt-get install python3-pip
+# music player
+sudo apt-get install audacious audacious-plugins
