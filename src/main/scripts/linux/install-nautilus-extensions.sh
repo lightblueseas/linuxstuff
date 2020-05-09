7aadf69467d265e9102b282349e317fdc1df58a4
James Phelps <james.phelps.imf.agent@gmail.com>
2020-05-09 13:09:26 +0200

    Update install-nautilus-extensions.sh

--- src/main/scripts/linux/install-nautilus-extensions.sh
+++ src/main/scripts/linux/install-nautilus-extensions.sh
@@ -1,1 +1,14 @@
-
+# nautilus extensions
+sudo apt-get install nautilus-actions
+sudo apt-get install nautilus-gksu
+sudo apt-get install nautilus-actions-extra
+sudo apt-get install nautilus-admin 
+sudo apt-get install nautilus-sendto 
+sudo apt-get install nautilus-image-converter 
+sudo apt-get install nautilus-compare 
+sudo apt-get install nautilus-wipe 
+sudo apt-get install seahorse-nautilus 
+sudo apt-get install nautilus-gtkhash 
+sudo apt-get install nautilus-share
+sudo apt-get install nautilus-script-manager
+sudo apt-get install ffmpegthumbnailer 
