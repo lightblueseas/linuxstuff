cd4c71c92bd60b879525c5c2d5abaacb7c982487
James Phelps <james.phelps.imf.agent@gmail.com>
2020-05-09 13:39:36 +0200

    Update and rename install-clamav.sh to install-antivirus-clamav.sh

---
+++ src/main/scripts/linux/install-antivirus-clamav.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# antivirus programm clamav
+sudo apt-get install clamav clamav-freshclam 
+sudo apt-get install clamav-docs
+sudo apt-get install clamav-daemon 
+sudo apt-get install clamtk 
+sudo apt-get install clamtk-nautilus
