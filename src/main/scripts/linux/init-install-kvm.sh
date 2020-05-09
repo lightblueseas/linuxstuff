3e2bcd3e4bdcf77253b40bff7fbd5f1ff06da0d0
James Phelps <james.phelps.imf.agent@gmail.com>
2020-05-09 13:35:54 +0200

    Update init-install-kvm.sh

--- src/main/scripts/linux/init-install-kvm.sh
+++ src/main/scripts/linux/init-install-kvm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get install qemu-kvm 
+sudo apt-get install libvirt-bin 
+sudo apt-get install ubuntu-vm-builder 
+sudo apt-get install bridge-utils
 sudo adduser `id -un` libvirtd
 # virsh list --all
 # sudo ls -la /var/run/libvirt/libvirt-sock
