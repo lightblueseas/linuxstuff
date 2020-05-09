7681a1352d864f1c11e48ec15beaa2a7eb5e516f
James Phelps <james.phelps.imf.agent@gmail.com>
2020-05-09 13:37:10 +0200

    Update init-linux-os-03.sh

--- src/main/scripts/linux/init-linux-os-03.sh
+++ src/main/scripts/linux/init-linux-os-03.sh
@@ -4,19 +4,7 @@ sudo apt-get install shotwell
 sudo apt-get install gnome-system-tools
 sudo apt-get install gnome-sushi 
 # nautilus extensions
-sudo apt-get install nautilus-actions
-sudo apt-get install nautilus-gksu
-sudo apt-get install nautilus-actions-extra
-sudo apt-get install nautilus-admin 
-sudo apt-get install nautilus-sendto 
-sudo apt-get install nautilus-image-converter 
-sudo apt-get install nautilus-compare 
-sudo apt-get install nautilus-wipe 
-sudo apt-get install seahorse-nautilus 
-sudo apt-get install nautilus-gtkhash 
-sudo apt-get install nautilus-share
-sudo apt-get install nautilus-script-manager
-sudo apt-get install ffmpegthumbnailer 
+./install-nautilus-extensions.sh
 # nodejs
 sudo apt install nodejs npm
 DIR="/usr/local/lib/node_modules"
