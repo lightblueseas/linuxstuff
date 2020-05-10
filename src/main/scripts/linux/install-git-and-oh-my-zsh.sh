737cc5105366c677dbb3adc8addb5c8bceefa728
Asterios Raptis <asterios.raptis@gmx.net>
2020-05-10 18:36:20 +0200

    added yes options

--- src/main/scripts/linux/install-git-and-oh-my-zsh.sh
+++ src/main/scripts/linux/install-git-and-oh-my-zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install git
-sudo apt-get install git-flow
-sudo apt-get install curl
+sudo apt-get install -y git
+sudo apt-get install -y git-flow
+sudo apt-get install -y curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
